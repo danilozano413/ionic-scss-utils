038ddc7714d10fda9b237f706d09d8d6f9f6df17
Dani <daniel@berebel.io>
2021-10-27 16:24:20 +0200

    Add compile scripts

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+    gulp.src('./**/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./**/*.scss', ['sass']);
+});
